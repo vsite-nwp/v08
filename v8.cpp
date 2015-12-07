a664267572b054bf1477417656bc009b641134fa
Nenad Caklovic <nenad.caklovic@avl.com>
2015-12-07 16:34:42 +0100

    Added project files

---
+++ v8.cpp
@@ -0,0 +1,19 @@
+#include "stdafx.h"
+#include "v8.h"
+#include "MainDlg.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#undef THIS_FILE
+static char THIS_FILE[] = __FILE__;
+#endif
+
+V8App theApp;
+
+BOOL V8App::InitInstance()
+{
+	MainDlg dlg;
+	m_pMainWnd = &dlg;
+	dlg.DoModal();
+	return FALSE;
+}
