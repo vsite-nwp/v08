6c8598a691e89d89dc6a62dfe75149d853a7a1df
Lidija Šikić <ligalic@vsite.hr>
2015-12-13 16:11:42 +0100

    gotovo

--- MainDlg.cpp
+++ MainDlg.cpp
@@ -17,17 +17,52 @@ MainDlg::MainDlg(CWnd* pParent /*=NULL*/)
 void MainDlg::DoDataExchange(CDataExchange* pDX)
 {
 	CDialog::DoDataExchange(pDX);
+	DDX_Control(pDX, IDC_LIST1, myVar);
 }
 
 BEGIN_MESSAGE_MAP(MainDlg, CDialog)
+	ON_BN_CLICKED(IDC_BUTTON1, &MainDlg::OnBnClickedButton1)
+	ON_BN_CLICKED(IDC_BUTTON2, &MainDlg::OnBnClickedButton2)
 END_MESSAGE_MAP()
 
 BOOL MainDlg::OnInitDialog()
 {
+	int n = 0;
 	CDialog::OnInitDialog();
 	SetIcon(m_hIcon, TRUE);			// Set big icon
 	SetIcon(m_hIcon, FALSE);		// Set small icon
-	
+	myVar.InsertColumn(0,"Title",LVCFMT_LEFT,120);
+	myVar.InsertColumn(1, "Year", LVCFMT_LEFT,60);
+	myVar.SetExtendedStyle(LVS_EX_FULLROWSELECT);
 	return TRUE;
 }
 
+
+
+void MainDlg::OnBnClickedButton1()
+{
+	
+	CString title, year;
+	int index = myVar.GetItemCount();
+	
+	GetDlgItemText(IDC_EDIT1, title);
+	GetDlgItemText(IDC_EDIT2, year);
+
+	myVar.InsertItem(index, title);
+	myVar.SetItemText(index, 1, year);
+
+	SetDlgItemText(IDC_EDIT1, "");
+	SetDlgItemText(IDC_EDIT2, "");
+}
+
+
+void MainDlg::OnBnClickedButton2()
+{
+	
+	int index;
+	index = myVar.GetItemCount();
+	if (index)
+	{
+		myVar.DeleteItem(myVar.GetNextItem(-1, LVNI_SELECTED));
+	}
+}
