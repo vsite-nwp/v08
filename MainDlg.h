948760bd0b76ecbfa7554686fd8d0347baa4b5a1
gajotres100 <pongo666>
2015-12-14 11:06:08 +0100

    Selektiranje cijelog reda i brisanje dodano

--- MainDlg.h
+++ MainDlg.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "resource.h"	
+#include "afxcmn.h"
 
 class MainDlg : public CDialog
 {
@@ -13,4 +14,9 @@ protected:
 	HICON m_hIcon;
 	BOOL OnInitDialog() override;
 	DECLARE_MESSAGE_MAP()
+public:
+	CListCtrl List;
+	afx_msg void OnBnClickedButton1();
+	afx_msg void OnBnClickedButton2();
+	afx_msg void OnNMClickList1(NMHDR *pNMHDR, LRESULT *pResult);
 };
