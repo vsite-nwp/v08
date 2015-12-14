97835594cca3eea7f81622227471db16c56cb88d
vanjs83 <tihomir.vanjurek@gmail.com>
2015-12-14 21:04:56 +0100

    Solve  all exercise!!

--- MainDlg.cpp
+++ MainDlg.cpp
@@ -10,6 +10,7 @@ static char THIS_FILE[] = __FILE__;
 
 MainDlg::MainDlg(CWnd* pParent /*=NULL*/)
 	: CDialog(MainDlg::IDD, pParent)
+	, movies(0)
 {
 	m_hIcon = AfxGetApp()->LoadIcon(IDR_MAINFRAME);
 }
@@ -17,17 +18,43 @@ MainDlg::MainDlg(CWnd* pParent /*=NULL*/)
 void MainDlg::DoDataExchange(CDataExchange* pDX)
 {
 	CDialog::DoDataExchange(pDX);
+	DDX_Control(pDX, IDC_LIST1, film);
 }
 
 BEGIN_MESSAGE_MAP(MainDlg, CDialog)
+	ON_BN_CLICKED(IDC_BUTTON1, &MainDlg::OnAdd)
+	ON_BN_CLICKED(IDC_BUTTON2, &MainDlg::OnDelete)
 END_MESSAGE_MAP()
 
 BOOL MainDlg::OnInitDialog()
 {
 	CDialog::OnInitDialog();
+	film.InsertColumn(0,_T("Title"),LVCFMT_JUSTIFYMASK, 120);
+	film.InsertColumn(1,_T("Year"), LVCFMT_LEFT, 100);
+	film.SetExtendedStyle(LVS_EX_FULLROWSELECT  );
 	SetIcon(m_hIcon, TRUE);			// Set big icon
 	SetIcon(m_hIcon, FALSE);		// Set small icon
 	
 	return TRUE;
 }
 
+
+
+void MainDlg::OnAdd()
+{
+	//  Add your control notification handler code here
+	CString s1, s2;
+	GetDlgItemText(IDC_EDIT1, s1);
+	GetDlgItemText(IDC_EDIT2, s2);
+	film.InsertItem(0, s1);
+	film.SetItemText(0, 1, s2);
+
+}
+
+
+void MainDlg::OnDelete()
+{
+	//  Add your control notification handler code here
+	film.DeleteItem(film.GetNextItem(-1, LVNI_SELECTED));
+
+}
