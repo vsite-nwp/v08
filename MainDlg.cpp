bf46e36b0fa6a0a7c2f32e0a8c9a5f86d49bde82
vanjs83 <tihomir.vanjurek@gmail.com>
2015-12-16 12:16:36 +0100

    Add number_item variable and check does item exist!!

--- MainDlg.cpp
+++ MainDlg.cpp
@@ -29,7 +29,7 @@ END_MESSAGE_MAP()
 BOOL MainDlg::OnInitDialog()
 {
 	CDialog::OnInitDialog();
-	film.InsertColumn(0,_T("Title"),LVCFMT_JUSTIFYMASK, 120);
+	film.InsertColumn(0,_T("Title"),LVCFMT_LEFT, 120);
 	film.InsertColumn(1,_T("Year"), LVCFMT_LEFT, 100);
 	film.SetExtendedStyle(LVS_EX_FULLROWSELECT  );
 	SetIcon(m_hIcon, TRUE);			// Set big icon
@@ -43,11 +43,11 @@ BOOL MainDlg::OnInitDialog()
 void MainDlg::OnAdd()
 {
 	//  Add your control notification handler code here
-	CString s1, s2;
+	CString s1, s2; int number_item=film.GetItemCount();
 	GetDlgItemText(IDC_EDIT1, s1);
 	GetDlgItemText(IDC_EDIT2, s2);
-	film.InsertItem(0, s1);
-	film.SetItemText(0, 1, s2);
+	film.InsertItem(number_item, s1);
+	film.SetItemText(number_item,1, s2);
 
 }
 
@@ -55,6 +55,9 @@ void MainDlg::OnAdd()
 void MainDlg::OnDelete()
 {
 	//  Add your control notification handler code here
+	int number_item = film.GetNextItem(-1, LVNI_SELECTED);
+	if (number_item > 0)
+		film.DeleteItem(number_item);
 	film.DeleteItem(film.GetNextItem(-1, LVNI_SELECTED));
 
 }
