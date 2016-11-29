7f0c87ff53279bab4e1e7ed8822f0a4ac90b00ac
Student <Student@VSITE>
2016-11-29 13:40:14 +0100

    Sve odjednom

--- MainDlg.cpp
+++ MainDlg.cpp
@@ -17,9 +17,12 @@ MainDlg::MainDlg(CWnd* pParent /*=NULL*/)
 void MainDlg::DoDataExchange(CDataExchange* pDX)
 {
 	CDialog::DoDataExchange(pDX);
+	DDX_Control(pDX, IDC_LIST1, list_control);
 }
 
 BEGIN_MESSAGE_MAP(MainDlg, CDialog)
+	ON_BN_CLICKED(IDC_BUTTON1, &MainDlg::add_row)
+	ON_BN_CLICKED(IDC_BUTTON2, &MainDlg::delete_row)
 END_MESSAGE_MAP()
 
 BOOL MainDlg::OnInitDialog()
@@ -28,6 +31,28 @@ BOOL MainDlg::OnInitDialog()
 	SetIcon(m_hIcon, TRUE);			// Set big icon
 	SetIcon(m_hIcon, FALSE);		// Set small icon
 	
+	list_control.InsertColumn(0, _T("Title"), 0, 100);
+	list_control.InsertColumn(1, _T("Year"), 0, 50);
+	list_control.SetExtendedStyle(list_control.GetExtendedStyle() | LVS_EX_FULLROWSELECT);
+	
 	return TRUE;
 }
 
+void MainDlg::add_row()
+{
+	// Add your control notification handler code here
+	CString title;
+	GetDlgItemText(IDC_EDIT1, title);
+	
+	int col_index = list_control.InsertItem(list_control.GetItemCount(), title);
+
+	GetDlgItemText(IDC_EDIT2, title);
+	list_control.SetItemText(col_index, 1, title);
+
+	SetDlgItemText(IDC_EDIT1, "");
+	SetDlgItemText(IDC_EDIT2, "");
+}
+
+void MainDlg::delete_row() {
+	list_control.DeleteItem(list_control.GetNextItem(-1, LVNI_SELECTED));
+}
--- MainDlg.h
+++ MainDlg.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "resource.h"	
+#include "afxcmn.h"
 
 class MainDlg : public CDialog
 {
@@ -13,4 +14,8 @@ protected:
 	HICON m_hIcon;
 	BOOL OnInitDialog() override;
 	DECLARE_MESSAGE_MAP()
+public:
+	CListCtrl list_control;
+	afx_msg void add_row();
+	afx_msg void delete_row();
 };
