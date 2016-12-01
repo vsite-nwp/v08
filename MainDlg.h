c2129a98dec4815929482b877bf103213928feca
Marijo Horvat <Marijo.Horvat@vsite.hr>
2016-12-01 18:40:05 +0100

    vjezba 8 zavrsena.

--- MainDlg.h
+++ MainDlg.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "resource.h"	
+#include "afxcmn.h"
 
 class MainDlg : public CDialog
 {
@@ -13,4 +14,8 @@ protected:
 	HICON m_hIcon;
 	BOOL OnInitDialog() override;
 	DECLARE_MESSAGE_MAP()
+public:
+	CListCtrl ListaFilmova;
+	afx_msg void OnAdd();
+	afx_msg void OnRemove();
 };
