c6f813409aa8c30deec8e15b1479d138d24216b6
Petar Domitrović <domitrovicpetar@gmail.com>
2017-11-29 19:06:49 +0100

    v08.

--- MainDlg.cpp
+++ MainDlg.cpp
@@ -17,9 +17,12 @@ MainDlg::MainDlg(CWnd* pParent /*=NULL*/)
 void MainDlg::DoDataExchange(CDataExchange* pDX)
 {
 	CDialog::DoDataExchange(pDX);
+	DDX_Control(pDX, IDC_LIST1, title);
 }
 
 BEGIN_MESSAGE_MAP(MainDlg, CDialog)
+	ON_BN_CLICKED(IDC_BUTTON1, &MainDlg::OnBnClickedButton1)
+	ON_BN_CLICKED(IDC_BUTTON2, &MainDlg::OnBnClickedButton2)
 END_MESSAGE_MAP()
 
 BOOL MainDlg::OnInitDialog()
@@ -27,7 +30,26 @@ BOOL MainDlg::OnInitDialog()
 	CDialog::OnInitDialog();
 	SetIcon(m_hIcon, TRUE);			// Set big icon
 	SetIcon(m_hIcon, FALSE);		// Set small icon
-	
+	title.InsertColumn(0, "Title", 0, 141, 0);
+	title.InsertColumn(1, "Year", 0, 50, 0);
+	title.SetExtendedStyle(LVS_EX_FULLROWSELECT);
 	return TRUE;
 }
 
+
+
+void MainDlg::OnBnClickedButton1()
+{
+	CString str;	
+	GetDlgItemText(IDC_EDIT1, str);
+	title.InsertItem(0, str);
+	GetDlgItemText(IDC_EDIT2, str);
+	title.SetItemText(0, 1,str);
+}
+
+
+
+void MainDlg::OnBnClickedButton2()
+{	
+	title.DeleteItem(title.GetNextItem(-1, LVNI_SELECTED));
+}
