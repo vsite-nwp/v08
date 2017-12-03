4531eb08fa6c4cd06118c161abad308c532ac4d3
Dijana Molnar <dijanamolnar89@gmail.com>
2017-12-03 17:57:47 +0100

    Dodani stupci u list kontrolu

--- MainDlg.cpp
+++ MainDlg.cpp
@@ -17,6 +17,7 @@ MainDlg::MainDlg(CWnd* pParent /*=NULL*/)
 void MainDlg::DoDataExchange(CDataExchange* pDX)
 {
 	CDialog::DoDataExchange(pDX);
+	DDX_Control(pDX, IDC_LIST1, movies);
 }
 
 BEGIN_MESSAGE_MAP(MainDlg, CDialog)
@@ -27,7 +28,10 @@ BOOL MainDlg::OnInitDialog()
 	CDialog::OnInitDialog();
 	SetIcon(m_hIcon, TRUE);			// Set big icon
 	SetIcon(m_hIcon, FALSE);		// Set small icon
-	
+	CString text = "Title";
+	movies.InsertColumn(0, (LPCTSTR)text, LVCFMT_LEFT,140);
+	text = "Year";
+	movies.InsertColumn(1, (LPCTSTR)text, LVCFMT_CENTER, 50);
 	return TRUE;
 }
 
--- MainDlg.h
+++ MainDlg.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "resource.h"	
+#include "afxcmn.h"
 
 class MainDlg : public CDialog
 {
@@ -13,4 +14,6 @@ protected:
 	HICON m_hIcon;
 	BOOL OnInitDialog() override;
 	DECLARE_MESSAGE_MAP()
+public:
+	CListCtrl movies;
 };
