5b3ccf6abfb70f2435e6209f7ec6507729c82177
Filip Kreč <filip.krec@yahoo.com>
2018-12-03 21:08:05 +0100

    Gotovo?

--- MainDlg.cpp
+++ MainDlg.cpp
@@ -1,6 +1,7 @@
 #include "stdafx.h"
 #include "v8.h"
 #include "MainDlg.h"
+#include "cstring"
 
 #ifdef _DEBUG
 #define new DEBUG_NEW
@@ -21,6 +22,8 @@ void MainDlg::DoDataExchange(CDataExchange* pDX)
 }
 
 BEGIN_MESSAGE_MAP(MainDlg, CDialog)
+	ON_BN_CLICKED(IDC_BUTTON1, &MainDlg::OnBnClickedButton1)
+	ON_BN_CLICKED(IDC_BUTTON2, &MainDlg::OnBnClickedButton2)
 END_MESSAGE_MAP()
 
 BOOL MainDlg::OnInitDialog()
@@ -28,9 +31,27 @@ BOOL MainDlg::OnInitDialog()
 	CDialog::OnInitDialog();
 	SetIcon(m_hIcon, TRUE);			// Set big icon
 	SetIcon(m_hIcon, FALSE);		// Set small icon
-	Control.InsertColumn(0,"Title", LVCFMT_LEFT,-1,-1);
-	Control.InsertColumn(1, "Year", LVCFMT_CENTER, -1, -1);
+	Control.SetExtendedStyle(LVS_EX_FULLROWSELECT);
+	Control.InsertColumn(0,"Title", LVCFMT_LEFT,120,20);
+	Control.InsertColumn(1, "Year", LVCFMT_CENTER, 50, 20);
 
 	return TRUE;
 }
 
+
+
+void MainDlg::OnBnClickedButton1()
+{
+	CString title;
+	CString year;
+	GetDlgItemText(IDC_EDIT1, title);
+	GetDlgItemText(IDC_EDIT2, year);
+	Control.InsertItem(Control.GetItemCount(), title);
+	Control.SetItemText(Control.GetItemCount()-1, 1, year);
+}
+
+
+void MainDlg::OnBnClickedButton2()
+{
+	Control.DeleteItem(Control.GetNextItem(-1, LVNI_SELECTED));
+}
