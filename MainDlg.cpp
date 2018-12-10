85f9bc893fbe8c7d2ee77625336fc0f726648436
Andro Bošnjak <andro.bosnjak172@gmail.com>
2018-12-10 14:43:46 +0100

    gotovo

--- MainDlg.cpp
+++ MainDlg.cpp
@@ -17,9 +17,12 @@ MainDlg::MainDlg(CWnd* pParent /*=NULL*/)
 void MainDlg::DoDataExchange(CDataExchange* pDX)
 {
 	CDialog::DoDataExchange(pDX);
+	DDX_Control(pDX, IDC_LIST1, lc);
 }
 
 BEGIN_MESSAGE_MAP(MainDlg, CDialog)
+	ON_BN_CLICKED(IDC_BUTTON1, &MainDlg::OnAddButton)
+	ON_BN_CLICKED(IDC_BUTTON2, &MainDlg::OnDeleteButton)
 END_MESSAGE_MAP()
 
 BOOL MainDlg::OnInitDialog()
@@ -28,6 +31,33 @@ BOOL MainDlg::OnInitDialog()
 	SetIcon(m_hIcon, TRUE);			// Set big icon
 	SetIcon(m_hIcon, FALSE);		// Set small icon
 	
+	lc.InsertColumn(0,"Title",0,130);
+	lc.InsertColumn(1, "Year", 0, 50);
+	lc.SetExtendedStyle(LVS_EX_FULLROWSELECT);
 	return TRUE;
 }
 
+void MainDlg::OnAddButton()
+{
+	CString title,year;
+	int i = lc.GetItemCount();
+
+	GetDlgItemText(IDC_EDIT1, title);
+	GetDlgItemText(IDC_EDIT2, year);
+	if (year != "" && title != "") {
+		lc.InsertItem(i, title);
+		lc.SetItemText(i, 1, year);
+	}
+	else {
+		MessageBox("Please enter both columns", "warning", MB_ICONWARNING);
+	}
+}
+
+
+void MainDlg::OnDeleteButton()
+{
+	int i = lc.GetNextItem(-1, LVNI_SELECTED);
+	if(i != -1){
+		lc.DeleteItem(i);
+	}
+}
--- MainDlg.h
+++ MainDlg.h
@@ -1,13 +1,16 @@
 #pragma once
 
-#include "resource.h"	
-
+#include "resource.h"
+#include "afxcmn.h"
 class MainDlg : public CDialog
 {
 public:
 	MainDlg(CWnd* pParent = NULL);	// standard constructor
 	enum { IDD = IDD_V8_DIALOG };
-
+	
+	CListCtrl lc;
+	void OnAddButton();
+	void OnDeleteButton();
 protected:
 	void DoDataExchange(CDataExchange* pDX) override;
 	HICON m_hIcon;
