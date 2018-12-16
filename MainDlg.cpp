c84fb7efa23156d8dae697d91ddd0c69879f45af
Matija Akrap <maakrap@gmail.com>
2018-12-16 19:16:01 +0100

    Pokušaj prvi

--- MainDlg.cpp
+++ MainDlg.cpp
@@ -17,9 +17,12 @@ MainDlg::MainDlg(CWnd* pParent /*=NULL*/)
 void MainDlg::DoDataExchange(CDataExchange* pDX)
 {
 	CDialog::DoDataExchange(pDX);
+	DDX_Control(pDX, IDC_LIST1, list1);
 }
 
 BEGIN_MESSAGE_MAP(MainDlg, CDialog)
+	ON_BN_CLICKED(IDC_BUTTON1, &MainDlg::OnBnClickedButton1)
+	ON_BN_CLICKED(IDC_BUTTON2, &MainDlg::OnBnClickedButton2)
 END_MESSAGE_MAP()
 
 BOOL MainDlg::OnInitDialog()
@@ -28,6 +31,32 @@ BOOL MainDlg::OnInitDialog()
 	SetIcon(m_hIcon, TRUE);			// Set big icon
 	SetIcon(m_hIcon, FALSE);		// Set small icon
 	
+	list1.InsertColumn(0,_T("Title"),LVCFMT_LEFT, 150);
+	list1.InsertColumn(1,_T("Year"),LVCFMT_CENTER, 50);
+
+	list1.SetExtendedStyle(LVS_EX_FULLROWSELECT);
+
 	return TRUE;
 }
 
+
+void MainDlg::OnBnClickedButton1()
+{
+	CString movie, year;
+
+	GetDlgItemText(IDC_EDIT1, movie);
+	GetDlgItemText(IDC_EDIT2, year);
+
+	if (movie != "" && year != "") {
+		list1.InsertItem(0, movie);
+		list1.SetItemText(0, 1, year);
+	}else
+		MessageBox("Nisu popunjena sva polja!","Upozorenje",MB_OK);
+
+}
+
+void MainDlg::OnBnClickedButton2()
+{
+		list1.DeleteItem(list1.GetNextItem(-1, LVNI_SELECTED));
+	
+}
