bee52e315df642d6716a0bf7844558c529b51523
Ivan <ivan.zoric98@gmail.com>
2019-12-23 15:11:47 +0100

    vj8 solution

--- MainDlg.cpp
+++ MainDlg.cpp
@@ -1,33 +1,55 @@
 #include "stdafx.h"
 #include "v8.h"
 #include "MainDlg.h"
-
+#include <ctype.h>
 #ifdef _DEBUG
 #define new DEBUG_NEW
 #undef THIS_FILE
 static char THIS_FILE[] = __FILE__;
 #endif
 
-MainDlg::MainDlg(CWnd* pParent /*=NULL*/)
+MainDlg::MainDlg(CWnd* pParent)
 	: CDialog(MainDlg::IDD, pParent)
 {
 	m_hIcon = AfxGetApp()->LoadIcon(IDR_MAINFRAME);
 }
 
-void MainDlg::DoDataExchange(CDataExchange* pDX)
+void MainDlg::DoDataExchange(CDataExchange* pDDX)
 {
-	CDialog::DoDataExchange(pDX);
+	CDialog::DoDataExchange(pDDX);
+	DDX_Control(pDDX, IDC_LIST1, items);
 }
 
 BEGIN_MESSAGE_MAP(MainDlg, CDialog)
+	ON_BN_CLICKED(IDC_BUTTON1, &MainDlg::add)
+	ON_BN_CLICKED(IDC_BUTTON2, &MainDlg::add)
 END_MESSAGE_MAP()
 
-BOOL MainDlg::OnInitDialog()
-{
-	CDialog::OnInitDialog();
-	SetIcon(m_hIcon, TRUE);			// Set big icon
-	SetIcon(m_hIcon, FALSE);		// Set small icon
-	
-	return TRUE;
+BOOL MainDlg::OnInitDialog(){
+	CDialog::OnInitDialog();		
+	SetIcon(m_hIcon, FALSE);		
+	items.InsertColumn(0, _T("Name"), LVCFMT_LEFT, 100);
+	items.InsertColumn(1, _T("Year"), LVCFMT_LEFT,92);
+	items.SetExtendedStyle(LVS_EX_FULLROWSELECT);
+	GotoDlgCtrl(GetDlgItem(IDC_EDIT1));
+	return FALSE;
 }
 
+void MainDlg::add(){
+	CString nm, yr;
+	GetDlgItemText(IDC_EDIT1, nm);
+	GetDlgItemText(IDC_EDIT2, yr);
+	if (nm.IsEmpty() || yr.IsEmpty()){ return; };
+		int num = items.GetItemCount();
+		items.InsertItem(num, nm);
+		items.SetItemText(num, 1, yr);
+		SetDlgItemText(IDC_EDIT1, "");
+		SetDlgItemText(IDC_EDIT2, "");
+		}
+void MainDlg::del(){
+	if (items.GetNextItem(-1, LVNI_SELECTED) != -1)
+		items.DeleteItem(items.GetNextItem(-1, LVNI_SELECTED));
+}
+void MainDlg::OnOK(){
+	add();
+}
--- MainDlg.h
+++ MainDlg.h
@@ -1,16 +1,21 @@
 #pragma once
-
 #include "resource.h"	
 
 class MainDlg : public CDialog
 {
 public:
-	MainDlg(CWnd* pParent = NULL);	// standard constructor
+	MainDlg(CWnd* pParent = NULL);	
 	enum { IDD = IDD_V8_DIALOG };
 
 protected:
 	void DoDataExchange(CDataExchange* pDX) override;
 	HICON m_hIcon;
 	BOOL OnInitDialog() override;
+	virtual void OnOK();
 	DECLARE_MESSAGE_MAP()
+
+public:
+	CListCtrl items;
+	afx_msg void add();
+	afx_msg void del();
 };
