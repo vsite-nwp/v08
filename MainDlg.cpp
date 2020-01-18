3d62b752bf372bd615a8a96424329ea90d1595cc
tleva <tleva@DESKTOP-J8PKM7S>
2020-01-18 09:50:48 +0100

    Gotova vjezba 8

--- MainDlg.cpp
+++ MainDlg.cpp
@@ -8,8 +8,7 @@
 static char THIS_FILE[] = __FILE__;
 #endif
 
-MainDlg::MainDlg(CWnd* pParent /*=NULL*/)
-	: CDialog(MainDlg::IDD, pParent)
+MainDlg::MainDlg(CWnd* pParent) : CDialog(MainDlg::IDD, pParent)
 {
 	m_hIcon = AfxGetApp()->LoadIcon(IDR_MAINFRAME);
 }
@@ -17,9 +16,13 @@ MainDlg::MainDlg(CWnd* pParent /*=NULL*/)
 void MainDlg::DoDataExchange(CDataExchange* pDX)
 {
 	CDialog::DoDataExchange(pDX);
+	DDX_Control(pDX, IDC_LIST1, listOfMovies);
 }
 
 BEGIN_MESSAGE_MAP(MainDlg, CDialog)
+
+	ON_BN_CLICKED(IDC_BUTTON1, &MainDlg::OnClickedButton1)
+	ON_BN_CLICKED(IDC_BUTTON2, &MainDlg::OnClickedButton2)
 END_MESSAGE_MAP()
 
 BOOL MainDlg::OnInitDialog()
@@ -27,7 +30,30 @@ BOOL MainDlg::OnInitDialog()
 	CDialog::OnInitDialog();
 	SetIcon(m_hIcon, TRUE);			// Set big icon
 	SetIcon(m_hIcon, FALSE);		// Set small icon
-	
+	listOfMovies.InsertColumn(0, _T("Name"), LVCFMT_LEFT, 120);
+	listOfMovies.InsertColumn(1, _T("Year"), LVCFMT_CENTER, 60);
+	listOfMovies.SetExtendedStyle(LVS_EX_FULLROWSELECT);
 	return TRUE;
 }
 
+void MainDlg::OnClickedButton1()
+{
+	CString name, year;
+	GetDlgItemText(IDC_EDIT1, name);
+	GetDlgItemText(IDC_EDIT2, year);
+
+	if (!name.IsEmpty() && !year.IsEmpty()) {
+		int redovi = listOfMovies.GetItemCount();
+		listOfMovies.InsertItem(redovi, name);
+		listOfMovies.SetItemText(redovi, 1, year);
+		SetDlgItemText(IDC_EDIT1, "");
+		SetDlgItemText(IDC_EDIT2, "");
+	}
+	
+}
+
+void MainDlg::OnClickedButton2()
+{
+	if (listOfMovies.GetNextItem(-1, LVNI_SELECTED) != -1)
+		listOfMovies.DeleteItem(listOfMovies.GetNextItem(-1, LVNI_SELECTED));
+}
--- MainDlg.h
+++ MainDlg.h
@@ -7,6 +7,9 @@ class MainDlg : public CDialog
 public:
 	MainDlg(CWnd* pParent = NULL);	// standard constructor
 	enum { IDD = IDD_V8_DIALOG };
+	CListCtrl listOfMovies;
+	void OnClickedButton1();
+	void OnClickedButton2();
 
 protected:
 	void DoDataExchange(CDataExchange* pDX) override;
