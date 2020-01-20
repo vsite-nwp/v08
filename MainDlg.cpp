b88be69589c3371e5198297300a013b2607fa325
Marko Bohač <mabohac@vsite.hr>
2020-01-20 18:00:04 +0100

    Gotova vjezba

--- MainDlg.cpp
+++ MainDlg.cpp
@@ -8,18 +8,21 @@
 static char THIS_FILE[] = __FILE__;
 #endif
 
-MainDlg::MainDlg(CWnd* pParent /*=NULL*/)
+MainDlg::MainDlg(CWnd* pParent)
 	: CDialog(MainDlg::IDD, pParent)
 {
 	m_hIcon = AfxGetApp()->LoadIcon(IDR_MAINFRAME);
 }
 
-void MainDlg::DoDataExchange(CDataExchange* pDX)
+void MainDlg::DoDataExchange(CDataExchange* pDDX)
 {
-	CDialog::DoDataExchange(pDX);
+	CDialog::DoDataExchange(pDDX);
+	DDX_Control(pDDX, IDC_LIST1, lista);
 }
 
 BEGIN_MESSAGE_MAP(MainDlg, CDialog)
+	ON_BN_CLICKED(IDC_BUTTON1, &MainDlg::OnClickedButton1)
+	ON_BN_CLICKED(IDC_BUTTON2, &MainDlg::OnClickedButton2)
 END_MESSAGE_MAP()
 
 BOOL MainDlg::OnInitDialog()
@@ -27,7 +30,34 @@ BOOL MainDlg::OnInitDialog()
 	CDialog::OnInitDialog();
 	SetIcon(m_hIcon, TRUE);			// Set big icon
 	SetIcon(m_hIcon, FALSE);		// Set small icon
-	
+	lista.InsertColumn(0, _T("Title"), LVCFMT_LEFT, 150);
+	lista.InsertColumn(1, _T("Year"), LVCFMT_LEFT, 50);
+	lista.SetExtendedStyle(LVS_EX_FULLROWSELECT);
 	return TRUE;
 }
 
+void MainDlg::OnClickedButton1()
+{
+	CString title, year;
+	GetDlgItemText(IDC_EDIT1, title);
+	GetDlgItemText(IDC_EDIT2, year);
+
+	if (!title.IsEmpty() && !year.IsEmpty()) {
+		int mCount = lista.GetItemCount();
+		lista.InsertItem(mCount, title);
+		lista.SetItemText(mCount, 1, year);
+		SetDlgItemText(IDC_EDIT1, "");
+		SetDlgItemText(IDC_EDIT2, "");
+		GotoDlgCtrl(GetDlgItem(IDC_EDIT1));
+	}
+}
+
+void MainDlg::OnClickedButton2()
+{
+	if (lista.GetNextItem(-1, LVNI_SELECTED) != -1)
+		lista.DeleteItem(lista.GetNextItem(-1, LVNI_SELECTED));
+}
+void MainDlg::OnOK()
+{
+	OnClickedButton1();
+}
--- MainDlg.h
+++ MainDlg.h
@@ -7,10 +7,13 @@ class MainDlg : public CDialog
 public:
 	MainDlg(CWnd* pParent = NULL);	// standard constructor
 	enum { IDD = IDD_V8_DIALOG };
-
+	CListCtrl lista;
+	afx_msg void OnClickedButton1();
+	afx_msg void OnClickedButton2();
 protected:
 	void DoDataExchange(CDataExchange* pDX) override;
 	HICON m_hIcon;
 	BOOL OnInitDialog() override;
+	virtual void OnOK();
 	DECLARE_MESSAGE_MAP()
 };
