6fc94795f468ce6c1c0a3c31bf764f092e20c2ee
Ante Žunić <anzunic@vsite.hr>
2021-05-04 15:51:34 +0200

    Vjezba 8 rjesenje

--- MainDlg.cpp
+++ MainDlg.cpp
@@ -17,9 +17,14 @@ MainDlg::MainDlg(CWnd* pParent /*=NULL*/)
 void MainDlg::DoDataExchange(CDataExchange* pDX)
 {
 	CDialog::DoDataExchange(pDX);
+	DDX_Control(pDX, IDC_LIST1, list);
+	DDX_Control(pDX, IDC_BUTTON1, buttonAdd);
+	DDX_Control(pDX, IDC_BUTTON2, buttonDelete);
 }
 
 BEGIN_MESSAGE_MAP(MainDlg, CDialog)
+	ON_COMMAND(IDC_BUTTON1, Add)
+	ON_COMMAND(IDC_BUTTON2, Del)
 END_MESSAGE_MAP()
 
 BOOL MainDlg::OnInitDialog()
@@ -27,7 +32,29 @@ BOOL MainDlg::OnInitDialog()
 	CDialog::OnInitDialog();
 	SetIcon(m_hIcon, TRUE);			// Set big icon
 	SetIcon(m_hIcon, FALSE);		// Set small icon
-	
+
+	list.InsertColumn(0, "Title");
+	list.SetColumnWidth(0, 150);
+
+	list.InsertColumn(1, "Year");
+	list.SetColumnWidth(1, 40);
+
 	return TRUE;
 }
 
+void MainDlg::Add() {
+	TCHAR Title[512], Year[512];
+
+	::GetDlgItemText(*this, IDC_EDIT1, Title, 512);
+	::GetDlgItemText(*this, IDC_EDIT2, Year, 512);
+
+	list.InsertItem(0, Title);
+	list.SetItemText(0, 1, Year);
+
+	SetDlgItemText(IDC_EDIT1, "");
+	SetDlgItemText(IDC_EDIT2, "");
+}
+
+void MainDlg::Del() {
+	list.DeleteItem(list.GetNextItem(-1, LVNI_SELECTED));
+}
--- MainDlg.h
+++ MainDlg.h
@@ -7,10 +7,13 @@ class MainDlg : public CDialog
 public:
 	MainDlg(CWnd* pParent = NULL);	// standard constructor
 	enum { IDD = IDD_V8_DIALOG };
+	CListCtrl list, buttonAdd, buttonDelete, title, year;
 
 protected:
 	void DoDataExchange(CDataExchange* pDX) override;
 	HICON m_hIcon;
 	BOOL OnInitDialog() override;
+	afx_msg void Add();
+	afx_msg void Del();
 	DECLARE_MESSAGE_MAP()
 };
