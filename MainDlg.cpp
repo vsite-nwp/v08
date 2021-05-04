4def682cefd5ea289b2b0e31d77285bdbf38efc2
Ivan Bitunjac <ivbitunj@vsite.hr>
2021-05-04 15:49:43 +0200

    Rješena v08

--- MainDlg.cpp
+++ MainDlg.cpp
@@ -17,9 +17,16 @@ MainDlg::MainDlg(CWnd* pParent /*=NULL*/)
 void MainDlg::DoDataExchange(CDataExchange* pDX)
 {
 	CDialog::DoDataExchange(pDX);
+	DDX_Control(pDX, IDC_LIST1, movie_list);
+	DDX_Control(pDX, IDC_BUTTON1, add_btn);
+	DDX_Control(pDX, IDC_BUTTON2, delete_btn);
+	DDX_Control(pDX, IDC_EDIT1, title_edit);
+	DDX_Control(pDX, IDC_EDIT2, year_edit);
 }
 
 BEGIN_MESSAGE_MAP(MainDlg, CDialog)
+	ON_BN_CLICKED(IDC_BUTTON1, &MainDlg::AddButtonClicked)
+	ON_BN_CLICKED(IDC_BUTTON2, &MainDlg::DeleteButtonClicked)
 END_MESSAGE_MAP()
 
 BOOL MainDlg::OnInitDialog()
@@ -27,7 +34,53 @@ BOOL MainDlg::OnInitDialog()
 	CDialog::OnInitDialog();
 	SetIcon(m_hIcon, TRUE);			// Set big icon
 	SetIcon(m_hIcon, FALSE);		// Set small icon
+
+	if (movie_list.InsertColumn(0, _T("Title"), LVCFMT_LEFT, 100) == -1) return FALSE;
+	if (movie_list.InsertColumn(1, _T("Year"), LVCFMT_CENTER, 60) == -1) return FALSE;
+	movie_list.SetExtendedStyle(LVS_EX_FULLROWSELECT);
 	
 	return TRUE;
 }
 
+
+
+void MainDlg::AddButtonClicked()
+{
+	CString title_to_add;
+	CString year_to_add;
+	if (GetDlgItemText(IDC_EDIT1, title_to_add) != 0 && GetDlgItemText(IDC_EDIT2, year_to_add) != 0)
+	{
+		int list_size = movie_list.GetItemCount();
+		movie_list.InsertItem(list_size, title_to_add);
+		movie_list.SetItemText(list_size, 1, year_to_add);
+		SetDlgItemText(IDC_EDIT1, "");
+		SetDlgItemText(IDC_EDIT2, "");
+	}
+	else 
+	{
+		MessageBox("Title or year cannot be empty", "Error inserting!", MB_OK); 
+		SetDlgItemText(IDC_EDIT1, "");
+		SetDlgItemText(IDC_EDIT2, "");
+	}
+}
+
+//LVIS_SELECTED
+
+void MainDlg::DeleteButtonClicked()
+{
+	int selected_row_index = movie_list.GetNextItem(-1, LVNI_SELECTED);
+	if (selected_row_index != -1) 
+	{
+		movie_list.DeleteItem(selected_row_index);
+
+		//TODO: Implement selecting row above the deleted row
+		/*if (selected_row_index > 0)
+		{
+			movie_list.SetItemState(selected_row_index - 1, LVNI_SELECTED, 0);
+		}*/
+		return;
+	}
+	MessageBox("No row is selected!", "Error!", MB_OK);
+
+
+}
--- MainDlg.h
+++ MainDlg.h
@@ -13,4 +13,12 @@ protected:
 	HICON m_hIcon;
 	BOOL OnInitDialog() override;
 	DECLARE_MESSAGE_MAP()
+public:
+	CListCtrl movie_list;
+	CButton add_btn;
+	CButton delete_btn;
+	CEdit title_edit;
+	CEdit year_edit;
+	afx_msg void AddButtonClicked();
+	afx_msg void DeleteButtonClicked();
 };
