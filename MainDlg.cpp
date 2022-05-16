39fcbba62dfb399c4c3e29e53e28a412c8a57d90
robilovric <robilovric7@gmail.com>
2022-05-16 16:40:16 +0200

    Resolve ListControl demo

--- MainDlg.cpp
+++ MainDlg.cpp
@@ -10,6 +10,7 @@ static char THIS_FILE[] = __FILE__;
 
 MainDlg::MainDlg(CWnd* pParent /*=NULL*/)
 	: CDialog(MainDlg::IDD, pParent)
+	, title(_T(""))
 {
 	m_hIcon = AfxGetApp()->LoadIcon(IDR_MAINFRAME);
 }
@@ -17,9 +18,12 @@ MainDlg::MainDlg(CWnd* pParent /*=NULL*/)
 void MainDlg::DoDataExchange(CDataExchange* pDX)
 {
 	CDialog::DoDataExchange(pDX);
+	DDX_Control(pDX, IDC_LIST1, List);
 }
 
 BEGIN_MESSAGE_MAP(MainDlg, CDialog)
+	ON_BN_CLICKED(IDC_BUTTON1, BtnAdd)
+	ON_BN_CLICKED(IDC_BUTTON2, BtnDelete)
 END_MESSAGE_MAP()
 
 BOOL MainDlg::OnInitDialog()
@@ -27,7 +31,34 @@ BOOL MainDlg::OnInitDialog()
 	CDialog::OnInitDialog();
 	SetIcon(m_hIcon, TRUE);			// Set big icon
 	SetIcon(m_hIcon, FALSE);		// Set small icon
-	
+	List.InsertColumn(0, _T("Title"), LVCFMT_LEFT, 156);
+	List.InsertColumn(1, _T("Year"), LVCFMT_LEFT, 100);
+
+	List.SetExtendedStyle(LVS_EX_FULLROWSELECT);
 	return TRUE;
+}
+void MainDlg::BtnAdd()
+{
+	CString movieName;
+	CString movieYear;
+
+	const int index = List.GetItemCount();
+
+	GetDlgItemText(IDC_EDIT1, movieName);
+	GetDlgItemText(IDC_EDIT2, movieYear);
+
+	if (movieName == "" || movieYear == "" || !atoi(movieYear)) // to be added year must be a number
+		return;
+
+	List.InsertItem(index, movieName);
+	List.SetItemText(index, 1, movieYear); // 1 represents the year column
+
+	SetDlgItemText(IDC_EDIT1, "");
+	SetDlgItemText(IDC_EDIT2, "");
+}
+
+void MainDlg::BtnDelete()
+{
+	List.DeleteItem(List.GetNextItem(-1, LVNI_SELECTED)); // -1 finds the first item that matches the flag specified
 }
 
--- MainDlg.h
+++ MainDlg.h
@@ -7,10 +7,15 @@ class MainDlg : public CDialog
 public:
 	MainDlg(CWnd* pParent = NULL);	// standard constructor
 	enum { IDD = IDD_V8_DIALOG };
+	CListCtrl List;
+	afx_msg void BtnAdd();
+	afx_msg void BtnDelete();
 
 protected:
 	void DoDataExchange(CDataExchange* pDX) override;
 	HICON m_hIcon;
 	BOOL OnInitDialog() override;
 	DECLARE_MESSAGE_MAP()
+public:
+	CString title;
 };
