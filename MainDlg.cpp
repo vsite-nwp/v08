ff29e4946ff436072c3a1a415cebf310a4c8e261
LukaPetranovic <lupetran@vsite.hr>
2022-06-11 23:12:29 +0200

    Resolved v08

--- MainDlg.cpp
+++ MainDlg.cpp
@@ -17,9 +17,12 @@ MainDlg::MainDlg(CWnd* pParent /*=NULL*/)
 void MainDlg::DoDataExchange(CDataExchange* pDX)
 {
 	CDialog::DoDataExchange(pDX);
+	DDX_Control(pDX, IDC_LIST1, movieList);
 }
 
 BEGIN_MESSAGE_MAP(MainDlg, CDialog)
+	ON_BN_CLICKED(IDC_BUTTON1, &MainDlg::OnAddItem)
+	ON_BN_CLICKED(IDC_BUTTON2, &MainDlg::OnDeleteItem)
 END_MESSAGE_MAP()
 
 BOOL MainDlg::OnInitDialog()
@@ -27,7 +30,35 @@ BOOL MainDlg::OnInitDialog()
 	CDialog::OnInitDialog();
 	SetIcon(m_hIcon, TRUE);			// Set big icon
 	SetIcon(m_hIcon, FALSE);		// Set small icon
+	movieList.InsertColumn(0, _T("Title"), LVCFMT_LEFT, 200);
+	movieList.InsertColumn(1, _T("Year"), LVCFMT_LEFT, 80);
+	movieList.SetExtendedStyle(LVS_EX_FULLROWSELECT);
 	
 	return TRUE;
 }
 
+
+
+void MainDlg::OnAddItem()
+{
+	CString titleSave, yearSave;
+	GetDlgItemText(IDC_EDIT1, titleSave);
+	GetDlgItemText(IDC_EDIT2, yearSave);
+
+	int movieCount = movieList.GetItemCount();
+
+	movieList.InsertItem(movieCount, titleSave);
+	movieList.SetItemText(movieCount, 1, yearSave);
+
+	SetDlgItemText(IDC_EDIT1, _T(""));
+	SetDlgItemText(IDC_EDIT2, _T(""));
+}
+
+void MainDlg::OnDeleteItem()
+{
+	int movieSelect = movieList.GetNextItem(-1, LVNI_SELECTED);
+	if (movieSelect == -1) {
+		return;
+	}
+	movieList.DeleteItem(movieSelect);
+}
--- MainDlg.h
+++ MainDlg.h
@@ -13,4 +13,9 @@ protected:
 	HICON m_hIcon;
 	BOOL OnInitDialog() override;
 	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnAddItem();
+	afx_msg void OnDeleteItem();
+private:
+	CListCtrl movieList;
 };
