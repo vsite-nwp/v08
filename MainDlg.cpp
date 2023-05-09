e23a7345608f95a08c570223749e564df322e7ca
louis20th <lujomirs@gmail.com>
2023-05-09 21:56:05 +0200

    Add OnAdd, OnDelete and handle selection.

--- MainDlg.cpp
+++ MainDlg.cpp
@@ -17,14 +17,45 @@ MainDlg::MainDlg(CWnd* pParent /*=NULL*/)
 void MainDlg::DoDataExchange(CDataExchange* pDX)
 {
 	CDialog::DoDataExchange(pDX);
+	DDX_Control(pDX, IDC_LIST1, listCtrl);
+}
+
+void MainDlg::OnAdd() {
+	CString fieldValue;
+	// add row and set movie title
+	GetDlgItemText(IDC_EDIT1, fieldValue);
+	listCtrl.InsertItem(listSize, fieldValue);
+
+	// add column and add year
+	GetDlgItemText(IDC_EDIT2, fieldValue);
+	listCtrl.SetItemText(listSize, 1, fieldValue);
+
+	// reset fields
+	SetDlgItemText(IDC_EDIT1, "");
+	SetDlgItemText(IDC_EDIT2, "");
+	++listSize;
+}
+
+void MainDlg::OnDelete() {
+	if (listSize > 0) {
+		// decrese size counter only if delete was successful
+		if(listCtrl.DeleteItem(listCtrl.GetNextItem(-1, LVNI_SELECTED)) != 0)
+			--listSize;
+		return;
+	}
 }
 
 BEGIN_MESSAGE_MAP(MainDlg, CDialog)
+	ON_BN_CLICKED(IDC_BUTTON1, MainDlg::OnAdd)
+	ON_BN_CLICKED(IDC_BUTTON2, MainDlg::OnDelete)
 END_MESSAGE_MAP()
 
 BOOL MainDlg::OnInitDialog()
 {
 	CDialog::OnInitDialog();
+	listCtrl.InsertColumn(0, "Title", LVCFMT_RIGHT, 100, -1);
+	listCtrl.InsertColumn(1, "Year", LVCFMT_RIGHT, 100, -1);
+	listCtrl.SetExtendedStyle(GetExStyle() | LVS_EX_FULLROWSELECT);
 	SetIcon(m_hIcon, TRUE);			// Set big icon
 	SetIcon(m_hIcon, FALSE);		// Set small icon
 	
--- MainDlg.h
+++ MainDlg.h
@@ -12,5 +12,10 @@ protected:
 	void DoDataExchange(CDataExchange* pDX) override;
 	HICON m_hIcon;
 	BOOL OnInitDialog() override;
+	void OnAdd();
+	void OnDelete();
 	DECLARE_MESSAGE_MAP()
+public:
+	CListCtrl listCtrl;
+	UINT32 listSize = 0;
 };
