e8d68a72a5e13c9a1477ed90c08c36f9ff82db66
Giancarlo <giancarlo.bedek@hotmail.com>
2023-05-30 15:24:58 +0200

    Finish TODOs

--- MainDlg.cpp
+++ MainDlg.cpp
@@ -17,17 +17,66 @@ MainDlg::MainDlg(CWnd* pParent /*=NULL*/)
 void MainDlg::DoDataExchange(CDataExchange* pDX)
 {
 	CDialog::DoDataExchange(pDX);
+	DDX_Control(pDX, IDC_LIST1, list_control);
 }
 
 BEGIN_MESSAGE_MAP(MainDlg, CDialog)
+	ON_BN_CLICKED(IDC_BUTTON1, MainDlg::OnAdd)
+	ON_BN_CLICKED(IDC_BUTTON2, MainDlg::OnDelete)
 END_MESSAGE_MAP()
 
 BOOL MainDlg::OnInitDialog()
 {
 	CDialog::OnInitDialog();
-	SetIcon(m_hIcon, TRUE);			// Set big icon
+	SetIcon(m_hIcon, TRUE);		// Set big icon
 	SetIcon(m_hIcon, FALSE);		// Set small icon
-	
+
+	list_control.InsertColumn(
+		0, 
+		"Title", 
+		LVCFMT_LEFT, 
+		100, 
+		-1);
+
+	list_control.InsertColumn(
+		1, 
+		"Year", 
+		LVCFMT_RIGHT, 
+		100, 
+		-1);
+
+	list_control.SetExtendedStyle( LVS_EX_FULLROWSELECT);
 	return TRUE;
+}
+
+void MainDlg::OnAdd() 
+{
+	CString fieldValue;
+	const int item_count = list_control.GetItemCount();
+
+	GetDlgItemText(IDC_EDIT1, fieldValue);
+	list_control.InsertItem(item_count, fieldValue);
+
+	GetDlgItemText(IDC_EDIT2, fieldValue);
+	list_control.SetItemText(item_count, 1, fieldValue);
+
+	list_size++;
+}
+
+void MainDlg::OnDelete() 
+{
+	if (list_size <= 0 || list_control.GetItemCount() <= 0)
+		return;
+
+	if (list_control.DeleteItem(list_control.GetNextItem(-1, LVNI_SELECTED)) != 0)
+	{
+		list_size--;
+	}
+
+	const int delete_item_id = list_control.GetNextItem(-1, LVNI_SELECTED);
+	if (delete_item_id != -1)
+	{
+		list_control.DeleteItem(delete_item_id);
+	}
 }
 
--- MainDlg.h
+++ MainDlg.h
@@ -12,5 +12,9 @@ protected:
 	void DoDataExchange(CDataExchange* pDX) override;
 	HICON m_hIcon;
 	BOOL OnInitDialog() override;
+	CListCtrl list_control;
+	uint32_t list_size;
 	DECLARE_MESSAGE_MAP()
+	void OnAdd();
+	void OnDelete();
 };
