f84eeb66eb9796168646056ee1a0fff21a6fd28a
Josip Grabušić <jograbus@vsite.hr>
2024-05-19 14:27:27 +0200

    Riješena vježba 8

--- MainDlg.cpp
+++ MainDlg.cpp
@@ -17,9 +17,15 @@ MainDlg::MainDlg(CWnd* pParent /*=NULL*/)
 void MainDlg::DoDataExchange(CDataExchange* pDX)
 {
 	CDialog::DoDataExchange(pDX);
+	DDX_Control(pDX, IDC_EDIT1, title_edit);
+	DDX_Control(pDX, IDC_EDIT2, year_edit);
+	DDX_Control(pDX, IDC_LIST1, movies_list);
 }
 
 BEGIN_MESSAGE_MAP(MainDlg, CDialog)
+	ON_BN_CLICKED(IDC_BUTTON1, &MainDlg::OnAddButtonClicked)
+	ON_BN_CLICKED(IDC_BUTTON2, &MainDlg::OnDeleteButtonClicked)
+	ON_BN_CLICKED(IDC_BUTTON3, &MainDlg::OnDeleteAllClicked)
 END_MESSAGE_MAP()
 
 BOOL MainDlg::OnInitDialog()
@@ -27,7 +33,38 @@ BOOL MainDlg::OnInitDialog()
 	CDialog::OnInitDialog();
 	SetIcon(m_hIcon, TRUE);			// Set big icon
 	SetIcon(m_hIcon, FALSE);		// Set small icon
-	
+	movies_list.InsertColumn(0, _T("Title"), LVCFMT_LEFT, 135);
+	movies_list.InsertColumn(1, _T("Year"), LVCFMT_LEFT, 55);
+	movies_list.SetExtendedStyle(movies_list.GetExtendedStyle() | LVS_EX_FULLROWSELECT);
 	return TRUE;
+}
+
+void MainDlg::OnAddButtonClicked()
+{
+	CString s;
+	title_edit.GetWindowText(s);
+	int n_item = movies_list.GetItemCount();
+	movies_list.InsertItem(n_item, s);
+	year_edit.GetWindowText(s);
+	movies_list.SetItemText(n_item, 1, s);
+	title_edit.SetWindowText("");
+	year_edit.SetWindowText("");
+
+}
+
+
+void MainDlg::OnDeleteButtonClicked()
+{
+	int selected = movies_list.GetNextItem(-1, LVNI_SELECTED);
+	if (selected != -1) {
+		movies_list.DeleteItem(selected);
+	}
+
+}
+
+
+void MainDlg::OnDeleteAllClicked()
+{
+	movies_list.DeleteAllItems();
 }
 
--- resource.h
+++ resource.h
@@ -9,6 +9,7 @@
 #define IDC_EDIT2                       1002
 #define IDC_BUTTON1                     1003
 #define IDC_BUTTON2                     1004
+#define IDC_BUTTON3                     1005
 
 // Next default values for new objects
 // 
