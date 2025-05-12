6e6dac72b9eef3eff5c6fc1695187ce16b04b8ba
Matej Marinkovic <matej261matej@gmail.com>
2025-05-12 12:33:50 +0200

    resolved

--- MainDlg.cpp
+++ MainDlg.cpp
@@ -1,4 +1,4 @@
-#include "stdafx.h"
+﻿#include "stdafx.h"
 #include "v8.h"
 #include "MainDlg.h"
 
@@ -17,9 +17,12 @@ MainDlg::MainDlg(CWnd* pParent /*=NULL*/)
 void MainDlg::DoDataExchange(CDataExchange* pDX)
 {
 	CDialog::DoDataExchange(pDX);
+	DDX_Control(pDX, IDC_LIST1, m_listCtrl);
 }
 
 BEGIN_MESSAGE_MAP(MainDlg, CDialog)
+	ON_BN_CLICKED(IDC_BUTTON1, &MainDlg::OnBnClickedButton1)
+	ON_BN_CLICKED(IDC_BUTTON2, &MainDlg::OnBnClickedButton2)
 END_MESSAGE_MAP()
 
 BOOL MainDlg::OnInitDialog()
@@ -27,6 +30,35 @@ BOOL MainDlg::OnInitDialog()
 	CDialog::OnInitDialog();
 	SetIcon(m_hIcon, TRUE);
 	SetIcon(m_hIcon, FALSE);
-	
+
+	m_listCtrl.SetExtendedStyle(LVS_EX_FULLROWSELECT | LVS_EX_GRIDLINES);
+
+	m_listCtrl.InsertColumn(0, _T("Title"), LVCFMT_LEFT, 80);
+	m_listCtrl.InsertColumn(1, _T("Year"), LVCFMT_LEFT, 50);
+
 	return TRUE;
+}
+
+void MainDlg::OnBnClickedButton1()
+{
+	CString strTitle, strYear;
+	GetDlgItemText(IDC_EDIT1, strTitle);
+	GetDlgItemText(IDC_EDIT2, strYear);
+
+	// Dodavanje novog reda u list kontrolu
+	int nItem = m_listCtrl.InsertItem(m_listCtrl.GetItemCount(), strTitle);
+	m_listCtrl.SetItemText(nItem, 1, strYear);
+
+	SetDlgItemText(IDC_EDIT1, _T(""));
+	SetDlgItemText(IDC_EDIT2, _T(""));
+}
+
+void MainDlg::OnBnClickedButton2()
+{
+	int nItem = m_listCtrl.GetNextItem(-1, LVNI_SELECTED);
+
+	if (nItem != -1)
+	{
+		m_listCtrl.DeleteItem(nItem);
+	}
 }
--- resource.h
+++ resource.h
@@ -9,6 +9,9 @@
 #define IDC_EDIT2                       1002
 #define IDC_BUTTON1                     1003
 #define IDC_BUTTON2                     1004
+#define IDC_LIST_CTRL                   1005
+#define IDC_BUTTON_ADD                  1008
+#define IDC_BUTTON_DELETE               1009  
 
 // Next default values for new objects
 // 
@@ -16,7 +19,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        129
 #define _APS_NEXT_COMMAND_VALUE         32771
-#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_CONTROL_VALUE         1012
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
