3be93224e2fccc44f02b3b51f0ec0d2be8f5e24a
Damir Magdić <damir.magdic@vsite.hr>
2025-05-16 15:21:02 +0200

    Lab08

--- MainDlg.cpp
+++ MainDlg.cpp
@@ -17,9 +17,14 @@ MainDlg::MainDlg(CWnd* pParent /*=NULL*/)
 void MainDlg::DoDataExchange(CDataExchange* pDX)
 {
 	CDialog::DoDataExchange(pDX);
+	DDX_Control(pDX, IDC_LIST1, m_listCtrl);
+	DDX_Control(pDX, IDC_EDIT1, m_edit1);
+	DDX_Control(pDX, IDC_EDIT2, m_edit2);
 }
 
 BEGIN_MESSAGE_MAP(MainDlg, CDialog)
+	ON_BN_CLICKED(IDC_BUTTON1, &MainDlg::OnBnClickedAdd)
+	ON_BN_CLICKED(IDC_BUTTON2, &MainDlg::OnBnClickedDelete)
 END_MESSAGE_MAP()
 
 BOOL MainDlg::OnInitDialog()
@@ -27,6 +32,34 @@ BOOL MainDlg::OnInitDialog()
 	CDialog::OnInitDialog();
 	SetIcon(m_hIcon, TRUE);
 	SetIcon(m_hIcon, FALSE);
+
+	m_listCtrl.SetExtendedStyle(LVS_EX_FULLROWSELECT);
+
+	m_listCtrl.InsertColumn(0, _T("Title"), LVCFMT_LEFT, 150);
+	m_listCtrl.InsertColumn(1, _T("Year"), LVCFMT_LEFT, 120);
 	
 	return TRUE;
+}
+
+void MainDlg::OnBnClickedAdd()
+{
+	CString title, year;
+	m_edit1.GetWindowText(title);
+	m_edit2.GetWindowText(year);
+
+	if (!title.IsEmpty() && !year.IsEmpty()) {
+		int index = m_listCtrl.InsertItem(m_listCtrl.GetItemCount(), title);
+		m_listCtrl.SetItemText(index, 1, year);
+
+		m_edit1.SetWindowText(_T(""));
+		m_edit2.SetWindowText(_T(""));
+	}
+}
+
+void MainDlg::OnBnClickedDelete()
+{
+	int selected = m_listCtrl.GetNextItem(-1, LVNI_SELECTED);
+	if (selected != -1) {
+		m_listCtrl.DeleteItem(selected);
+	}
 }
--- MainDlg.h
+++ MainDlg.h
@@ -13,4 +13,12 @@ protected:
 	HICON m_hIcon;
 	BOOL OnInitDialog() override;
 	DECLARE_MESSAGE_MAP()
+
+private:
+	CListCtrl m_listCtrl;
+	CEdit m_edit1;
+	CEdit m_edit2;
+
+	afx_msg void OnBnClickedAdd();
+	afx_msg void OnBnClickedDelete();
 };
