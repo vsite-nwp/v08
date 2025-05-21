24cba71b6595314d03e9175058ff91ea620a0513
rrasp94 <ruraspud@vsite.hr>
2025-05-21 19:26:59 +0200

    v08 solved

--- MainDlg.cpp
+++ MainDlg.cpp
@@ -17,9 +17,14 @@ MainDlg::MainDlg(CWnd* pParent /*=NULL*/)
 void MainDlg::DoDataExchange(CDataExchange* pDX)
 {
 	CDialog::DoDataExchange(pDX);
+	DDX_Control(pDX, IDC_LIST1, m_movieList);
+	DDX_Control(pDX, IDC_EDIT1, m_titleInput);
+	DDX_Control(pDX, IDC_EDIT2, m_yearInput);
 }
 
 BEGIN_MESSAGE_MAP(MainDlg, CDialog)
+	ON_BN_CLICKED(IDC_BUTTON1, &MainDlg::OnAddClicked)
+	ON_BN_CLICKED(IDC_BUTTON2, &MainDlg::OnRemoveClicked)
 END_MESSAGE_MAP()
 
 BOOL MainDlg::OnInitDialog()
@@ -27,6 +32,35 @@ BOOL MainDlg::OnInitDialog()
 	CDialog::OnInitDialog();
 	SetIcon(m_hIcon, TRUE);
 	SetIcon(m_hIcon, FALSE);
+
+	m_movieList.SetExtendedStyle(LVS_EX_FULLROWSELECT | LVS_EX_GRIDLINES);
+	m_movieList.InsertColumn(0, _T("Title"), LVCFMT_LEFT, 150);
+	m_movieList.InsertColumn(1, _T("Year"), LVCFMT_LEFT, 80);
 	
 	return TRUE;
+}
+
+void MainDlg::OnAddClicked()
+{
+	CString title, year;
+	m_titleInput.GetWindowText(title);
+	m_yearInput.GetWindowText(year);
+
+	title.Trim();
+	year.Trim();
+
+	if (!title.IsEmpty() && !year.IsEmpty()) {
+		int index = m_movieList.InsertItem(m_movieList.GetItemCount(), title);
+		m_movieList.SetItemText(index, 1, year);
+		m_titleInput.SetWindowText(_T(""));
+		m_yearInput.SetWindowText(_T(""));
+	}
+}
+
+void MainDlg::OnRemoveClicked()
+{
+	int selectedIndex = m_movieList.GetNextItem(-1, LVNI_SELECTED);
+	if (selectedIndex != -1) {
+		m_movieList.DeleteItem(selectedIndex);
+	}
 }
