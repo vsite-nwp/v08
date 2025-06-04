22013bdf36683455960f79769d24fb7f4be3db34
gg307 <gg307@email.com>
2025-06-04 19:56:57 +0200

    add

--- MainDlg.cpp
+++ MainDlg.cpp
@@ -8,6 +8,7 @@
 static char THIS_FILE[] = __FILE__;
 #endif
 
+
 MainDlg::MainDlg(CWnd* pParent /*=NULL*/)
 	: CDialog(MainDlg::IDD, pParent)
 {
@@ -17,16 +18,44 @@ MainDlg::MainDlg(CWnd* pParent /*=NULL*/)
 void MainDlg::DoDataExchange(CDataExchange* pDX)
 {
 	CDialog::DoDataExchange(pDX);
+	DDX_Control(pDX, IDC_LIST1, list_control);
 }
 
 BEGIN_MESSAGE_MAP(MainDlg, CDialog)
+	ON_BN_CLICKED(IDC_BUTTON1, AddItem)
+	ON_BN_CLICKED(IDC_BUTTON2, DeleteItem)
 END_MESSAGE_MAP()
 
+void MainDlg::AddItem() {
+	CString title;
+	CString year;
+
+	GetDlgItemText(IDC_EDIT1, title);
+	GetDlgItemText(IDC_EDIT2, year);
+
+	auto count = list_control.GetItemCount();
+
+	if (title && atoi(year)) {
+		list_control.InsertItem(count, title);
+		list_control.SetItemText(count, 1, year);
+	}
+}
+
+void MainDlg::DeleteItem() {
+	auto index = list_control.GetNextItem(NULL, LVNI_SELECTED);
+	if (index != NULL) {
+		list_control.DeleteItem(index);
+	}
+}
 BOOL MainDlg::OnInitDialog()
 {
 	CDialog::OnInitDialog();
 	SetIcon(m_hIcon, TRUE);
 	SetIcon(m_hIcon, FALSE);
-	
+
+	list_control.InsertColumn(0, "Title", LVCFMT_LEFT, 100);
+	list_control.InsertColumn(1, "Year", LVCFMT_LEFT, 150);
+	list_control.SetExtendedStyle(LVS_EX_FULLROWSELECT);
+
 	return TRUE;
 }
